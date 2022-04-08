f644ff444f1b913ca289aea0bf28e262973cfaa6
Evolusin <98921577+Evolusin@users.noreply.github.com>
2022-04-08 21:26:46 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Holding all game settings"""
+
+    def __init__(self):
+        """Initilize game settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
