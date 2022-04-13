75cb4f8ca36a5282d027958e4b9ef3440c8a9273
Evolusin <evoarcadias@gmail.com>
2022-04-13 17:22:31 +0200

    Moving left/right and range limit

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,12 +20,23 @@ class AlienInvasion:
         while True:
             self._check_events()
             self._update_screen()
+            self.ship.update()
     def _check_events(self):
         """Responds to keypresses and mouse events"""
         # Watch for keyboard and mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left= False
     def _update_screen(self):
         """Update and refresh screen"""
         # Set up filling screen
--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self,ai_game):
         """Initialize ship and set his position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #load ship
@@ -13,7 +14,22 @@ class Ship:
         self.rect = self.image.get_rect()
         # Start ship on bottom screem
         self.rect.midbottom = self.screen_rect.midbottom
+        # Store decimal number for ship horizontal position
+        self.x = float(self.rect.x)
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
 
     def blitme(self):
         """Draw ship at current location"""
         self.screen.blit(self.image,self.rect)
+
+    def update(self):
+        """Update ship position based on moving flag"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
